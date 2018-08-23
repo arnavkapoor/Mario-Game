66b431e3261f628dd7787a278b7d248c321d9975
Arnav Kapoor <arnavk805@gmail.com>
2018-08-23 18:52:22 +0100

    scenery update

--- code/images/t.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main(int argc, char const *argv[])
-{
-	for (int i=0;1<1e9;i++)
-		continue;
-	return 0;
-}
--- code/scenery.py
+++ code/scenery.py
@@ -5,10 +5,32 @@ class DrawScenery:
         with open("./images/clouds.txt") as fileobj:
             for line in fileobj:  
                 mycloud.append(line.strip('\n'))
-        return mycloud;
+        return mycloud
+    
     def drawmountains():
         mymountain=[]
         with open("./images/mountains.txt") as fileobj:
             for line in fileobj:  
                 mymountain.append(line.strip('\n'))
         return mymountain
+    
+    def drawpipes():
+        mypipes=[]
+        with open("./images/pipes.txt") as fileobj:
+            for line in fileobj:  
+                mypipes.append(line.strip('\n'))
+        return mypipes
+    
+    def drawbricks():
+        mybricks=[]
+        with open("./images/bricks.txt") as fileobj:
+            for line in fileobj:  
+                mybricks.append(line.strip('\n'))
+        return mybricks
+    
+    def drawbossenemy():
+        mybossenemy=[]
+        with open("./images/bossenemy.txt") as fileobj:
+            for line in fileobj:  
+                mybossenemy.append(line.strip('\n'))
+        return mybossenemy;
